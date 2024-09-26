b04714aa19203dee44df8416149764f4a918ee70
JustNekoChris <103611606+JustNekoChris@users.noreply.github.com>
2024-09-26 00:30:22 -0700

    made java project with required dependencies

---
+++ api/src/main/java/com/project2/group14/demo/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project2.group14.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+}
