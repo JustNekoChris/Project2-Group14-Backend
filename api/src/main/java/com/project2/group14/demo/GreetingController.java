5c54c7692f6f1ee880f7179436ea0cf3b68cd801
zaheemsmith <41562535+zaheemsmith@users.noreply.github.com>
2024-10-03 11:30:28 -0700

    working build

--- api/src/main/java/com/project2/group14/demo/GreetingController.java
+++ api/src/main/java/com/project2/group14/demo/GreetingController.java
@@ -11,8 +11,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
-public class Controller {
-    /*
+public class GreetingController {
+    
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
@@ -20,9 +20,9 @@ public class Controller {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
-    */
+    
 
-    @GetMapping("/data") //dummy route
+    /*@GetMapping("/data") //dummy route
     public ResponseEntity<List<MyEntity>> getData() {
         List<MyEntity> data = // fetch data from the database
         return ResponseEntity.ok(data);
@@ -33,16 +33,17 @@ public class Controller {
         MyEntity savedEntity = // save entity to the database
         return ResponseEntity.status(HttpStatus.CREATED).body(savedEntity);
     }
+        */
     
     }
 
    
    
-    @Configuration
-public class WebConfig implements WebMvcConfigurer {
+//     @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000"); //allows React Native application running on http://localhost:3000 to communicate with Spring Boot backend running on http://localhost:8080
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**").allowedOrigins("http://localhost:3000"); //allows React Native application running on http://localhost:3000 to communicate with Spring Boot backend running on http://localhost:8080
+//     }
+// }
