4a6a4fa994a29173444d69feeaa341b6f07fa5e2
zaheemsmith <41562535+zaheemsmith@users.noreply.github.com>
2024-10-10 09:02:29 -0700

    added JPA/MariaDB dependencies to build.gradle. Updated application.properties. Added DB Tables as classes

---
+++ src/main/java/com/project2/group14/demo/entity/Products.java
@@ -0,0 +1,10 @@
+// package com.project2.group14.demo.entity;
+
+// public class Products {
+//     Long productID;
+//     String name;
+//     String link;
+//     int amountWanted;
+//     int amountBought;
+//     boolean bought;
+// }
