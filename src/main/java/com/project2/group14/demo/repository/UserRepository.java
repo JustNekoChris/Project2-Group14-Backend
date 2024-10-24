a1fba2ff872b2c3b9941502e50aa7d6e6186d6fb
JustNekoChris <chbarajas20@gmail.com>
2024-10-24 15:44:00 -0700

    fixed small issues

--- src/main/java/com/project2/group14/demo/repository/UserRepository.java
+++ src/main/java/com/project2/group14/demo/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.project2.group14.demo.repository;
 
 import com.project2.group14.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // Methos to find users by name (case-sensitive)
-    List<User> findByNameContaining(String name);
+    // Method to find users by name (case-sensitive)
+    @Query("SELECT u FROM User u WHERE u.name LIKE CONCAT('%', :name, '%')")
+    List<User> findByNameContaining(@Param(value = "name") String name);
 }
--- src/main/java/com/project2/group14/demo/repository/WishlistsRepository.java
+++ src/main/java/com/project2/group14/demo/repository/WishlistsRepository.java
@@ -3,10 +3,11 @@ package com.project2.group14.demo.repository;
 import com.project2.group14.demo.entity.Wishlists;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface WishlistsRepository extends JpaRepository<Wishlists, Integer> {
-    @Qeury("SELECT * FROM wishlists WHERE userID = :userID")
+    @Query("SELECT w FROM Wishlists w WHERE w.userID = :userID")
     List<Wishlists> findWishlistsByUserID(@Param("userID") Integer userID);
 }
