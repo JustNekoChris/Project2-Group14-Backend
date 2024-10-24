a557161dcac5374a1317139a4f6aafa4739b976d
JustNekoChris <103611606+JustNekoChris@users.noreply.github.com>
2024-10-24 00:42:01 -0700

    Update WishlistsRepository.java

--- src/main/java/com/project2/group14/demo/repository/WishlistsRepository.java
+++ src/main/java/com/project2/group14/demo/repository/WishlistsRepository.java
@@ -4,4 +4,6 @@ import com.project2.group14.demo.entity.Wishlists;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishlistsRepository extends JpaRepository<Wishlists, Integer> {
+  @Qeury("SELECT * FROM wishlists WHERE userID = :userID")
+  List<Wishlists> findWishlistsByUserID(@Param("userID") Integer userID)
 }
