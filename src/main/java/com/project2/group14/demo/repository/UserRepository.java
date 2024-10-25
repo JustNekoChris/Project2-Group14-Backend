9e21eca5a19948e0afa65012a6c33428fcc5ddae
ponc3138 <fponce@csumb.edu>
2024-10-24 22:07:06 -0700

    Login Routes

--- src/main/java/com/project2/group14/demo/repository/UserRepository.java
+++ src/main/java/com/project2/group14/demo/repository/UserRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     // Method to find users by name (case-sensitive)
     @Query("SELECT u FROM User u WHERE u.name LIKE CONCAT('%', :name, '%')")
     List<User> findByNameContaining(@Param(value = "name") String name);
+
+    // Finds a user by their email address (case-sensitive)
+    Optional<User> findByEmail(String email);
 }
